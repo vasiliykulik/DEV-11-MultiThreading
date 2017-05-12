6c117eaf5286c0b91cca5e9f4f33a601e4e45b5c
Vasiliy Kylik <detroit.vv@gmail.com>
2017-05-12 09:32:43 +0300

    dead lock

--- src/main/java/Deadlock.java
+++ src/main/java/Deadlock.java
@@ -1,53 +1,70 @@
 package main.java;
 
+import java.util.Random;
+
 /**
  * Created by Vasiliy Kylik on 12.05.2017.
  */
 public class Deadlock {
-
   /*dead lock возникает случайно, поэтому нам нужно несколько раз  по разному вызвать transfer*/
 
-  public static void main(String[] args) {
-    new Thread()
-  }
 
-  public static void transfer(Account source, Account target, long amount) {
-    synchronized (source) {
-      synchronized (target) {
-        if (source.getBalance() >= amount) {
-          source.withdraw(amount);
-          target.put(amount);
+    private static Random random = new Random();
+    private static Account account1 = new Account(100L);
+    private static Account account2 = new Account(200L);
+
+    public static void main(String[] args) {
+        new Thread(new Worker()).start();
+        new Thread(new Worker()).start();
+    }
+
+    public static void transfer(Account source, Account target, long amount) {
+        synchronized (source) {
+            synchronized (target) {
+                if (source.getBalance() >= amount) {
+                    source.withdraw(amount);
+                    target.put(amount);
+                    System.out.println("Transfer: " + amount);
+                } else {
+                    System.out.println("Not enough money");
+                }
+            }
         }
-      }
-    }
-  }
-
-  public static class Worker implements Runnable{
-
-    @Override
-    public void run() {
-
-    }
-  }
-
-  /*Представим себе банковский акаунт, напишем его представление в коде*/
-  public static class Account {
-    private long balance;
-
-    public Account(long balance) {
-      this.balance = balance;
     }
 
-    public long getBalance() {
-      return balance;
+    public static class Worker implements Runnable {
+
+        @Override
+        public void run() {
+            /*Что бы  такая ситуация получилась - надо чтобы они друг на друга залокались и бесконечно ждут*/
+            while (true) {
+                if (random.nextBoolean()) {
+                    transfer(account1, account2, random.nextInt(10));
+                } else {
+                    transfer(account2, account1, random.nextInt(10));
+                }
+            }
+        }
     }
 
-    public void put(long amount) {
-      balance += amount;
-    }
+    /*Представим себе банковский акаунт, напишем его представление в коде*/
+    public static class Account {
+        private long balance;
 
-    public void withdraw(long amount) {
-      balance -= amount;
+        public Account(long balance) {
+            this.balance = balance;
+        }
+
+        public long getBalance() {
+            return balance;
+        }
+
+        public void put(long amount) {
+            balance += amount;
+        }
+
+        public void withdraw(long amount) {
+            balance -= amount;
+        }
     }
-  }
 }
