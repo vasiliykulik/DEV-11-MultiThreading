735ac0b22c6b9862311a3282e14593e8528ebfce
Raketa <detroi.vv@gmail.com>
2017-05-14 23:21:22 +0300

    CyclicBarrier Exchanger

--- src/main/java/locks/Synchronizers.java
+++ src/main/java/locks/Synchronizers.java
@@ -1,7 +1,10 @@
 package main.java.locks;
 
+import java.util.Random;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Exchanger;
+import java.util.stream.IntStream;
 
 /**
  * Created by Vasiliy Kylik on 14.05.2017.
@@ -11,12 +14,12 @@ public class Synchronizers {
 
   public static void main(String[] args) throws InterruptedException {
     Synchronizers synchronizers = new Synchronizers();
-    synchronizers.testCyclicBarrier();
+    synchronizers.testExchanger();
   }
 
   public void testCyclicBarrier() throws InterruptedException {
     /*не надо пересоздавать каждый раз*/
-    CyclicBarrier barrier = new CyclicBarrier(5,()-> System.out.println("Barrier exceeded"));
+    CyclicBarrier barrier = new CyclicBarrier(5, () -> System.out.println("Barrier exceeded"));
     while (true) {
       new Thread(() -> {
         /*Подвесит наши потоки*/
@@ -32,5 +35,21 @@ public class Synchronizers {
       }).start();
       Thread.sleep(1000);
     }
+  }
+
+  public void testExchanger() {
+
+    Exchanger<String> exchanger = new Exchanger<>();
+    IntStream.range(0, 2).forEach((i) -> new Thread(() -> {
+      try {
+        System.out.printf("");
+        Thread.sleep(new Random().nextInt(1000));
+        String name = Thread.currentThread().getName();
+        System.out.println(name + " ready to exchange");
+        System.out.println(name + " < - > " + exchanger.exchange(name));
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }).start());
   }
 }
