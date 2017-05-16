570f1f783710bac44813ea7b0b13dcdd54b64522
Raketa <detroi.vv@gmail.com>
2017-05-17 00:54:11 +0300

    testExecutor() testSubmit() ExecutorService

--- src/main/java/executors/ExecutorsExample.java
+++ src/main/java/executors/ExecutorsExample.java
@@ -1,24 +1,41 @@
 package main.java.executors;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * Created by Vasiliy Kylik on 16.05.2017.
  */
 public class ExecutorsExample {
 
-  public static void main(String[] args) {
-    new ExecutorsExample().testExecutor();
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
+    new ExecutorsExample().testSubmit();
+    /*new ExecutorsExample().testExecutor();*/
   }
 
   public void testExecutor() {
     Executor executor = Executors.newSingleThreadExecutor();
+    System.out.println(Thread.currentThread().getName() + " submits task");
     executor.execute(new Runnable() {
       @Override
       public void run() {
-        System.out.println("Async task started");
+        System.out.println(Thread.currentThread().getName() + " Async task started");
       }
     });
+  }
+
+  public void testSubmit() throws ExecutionException, InterruptedException {
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    Future<String> f = executorService.submit(new Callable<String>() {
+      @Override
+      public String call() throws Exception {
+        Thread.sleep(1000);
+        return "Task executed";
+      }
+    });
+    System.out.println("Waiting for result");
+    System.out.println("result " + f.get());
+    f.get();
+    executorService.shutdown();
   }
 }
